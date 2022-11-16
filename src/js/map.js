8cc492176d1a0cbdbf99b5c1fbe6c666c765fac9
shamma <shammacode@gmail.com>
2022-11-16 06:56:26 +0300

    init: add map

---
+++ src/js/map.js
@@ -0,0 +1,41 @@
+ymaps.ready(function () {
+    let footer = document.querySelector('footer')
+    let hidden = document.querySelector('.hidden')
+
+    var myMap = new ymaps.Map('myMap', {
+        center: [55.700359, 37.740776],
+        zoom: 18,
+    });
+
+    var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        balloonContentBody: [
+            '<address>',
+            '<strong>Территория Баланса</strong>',
+            '<br/>',
+            'г. Москва, ул. Юных Ленинцев, д.25',
+            '<br/>',
+            '+7-925-221-99-29',
+            '<br/>',
+            'Email : <a href="mailto:info@terrabalans.ru">info@terrabalans.ru</a>',
+            '</address>'
+        ].join('')
+    }, {
+        preset: 'islands#blueIcon',
+        iconCaptionMaxWidth: '150',
+    });
+
+    myMap.geoObjects.add(myPlacemark)
+
+    myPlacemark.events.add(['click', 'balloonclose'], function (e){
+        if(e.get('type')==='click'){
+            hidden.classList.remove('b-hide')
+            hidden.classList.add('b-show')
+        }
+        else if(e.get('type')==='balloonclose'){
+            hidden.classList.add('b-hide')
+            hidden.classList.remove('b-show')
+        }
+    })
+});
+
+
